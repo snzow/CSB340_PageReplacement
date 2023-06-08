edd3dc3545553f6746250270107fa2c31bdd6112
snzow <91493272+snzow@users.noreply.github.com>
2023-06-07 21:34:10 -0700

    a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,35 +29,40 @@ public class Main {
         int optTotal = 0;
         int fifoTotal = 0;
         int lruTotal = 0;
-
-        for(int refSize = 10; refSize <= 20; refSize += 5){
-            int[] gen = p.getReferenceString(refSize);
-            System.out.println("------------------------");
-            System.out.println("REFSIZE: " + refSize);
-            System.out.println(Arrays.toString(gen));
-            for(int numFrames = 3; numFrames <= 7; numFrames += 2){
-                int optFault = opt.run(gen,numFrames);
-                int fifoFault = fifo.run(gen,numFrames);
-                int lruFault = lru.run(gen,numFrames);
-                optTotal += optFault;
-                fifoTotal += fifoFault;
-                lruTotal += lruFault;
-                System.out.println();
-                System.out.println("Faults for " + numFrames + " page frames");
-                System.out.println("Optimal: " + optFault);
-                System.out.println("First In First Out: " + fifoFault);
-                System.out.println("Least Recently Used: " + lruFault);
-                System.out.println();
+        int v = 0;
+        double times = 0;
+        while(v < 100000) {
+            for (int refSize = 10; refSize <= 20; refSize += 5) {
+                int[] gen = p.getReferenceString(refSize);
+                System.out.println("------------------------");
+                System.out.println("REFSIZE: " + refSize);
+                System.out.println(Arrays.toString(gen));
+                for (int numFrames = 3; numFrames <= 7; numFrames += 2) {
+                    int optFault = opt.run(gen, numFrames);
+                    int fifoFault = fifo.run(gen, numFrames);
+                    int lruFault = lru.run(gen, numFrames);
+                    optTotal += optFault;
+                    fifoTotal += fifoFault;
+                    lruTotal += lruFault;
+                    System.out.println();
+                    System.out.println("Faults for " + numFrames + " page frames");
+                    System.out.println("Optimal: " + optFault);
+                    System.out.println("First In First Out: " + fifoFault);
+                    System.out.println("Least Recently Used: " + lruFault);
+                    System.out.println();
+                    times++;
+                }
             }
+            v++;
         }
 
-        double optAvg = optTotal / 27.0;
-        double fifoAvg = fifoTotal / 27.0;
-        double lruAvg = lruTotal / 27.0;
+        double optAvg = optTotal / times;
+        double fifoAvg = fifoTotal / times;
+        double lruAvg = lruTotal / times;
         System.out.println("-Averages-");
-        System.out.println("Optimal : " + Math.round(optAvg * 100.0) / 100.0);
-        System.out.println("FIFO : " + Math.round(fifoAvg * 100.0) / 100.0);
-        System.out.println("LRU : " + Math.round(lruAvg * 100.0) / 100.0);
+        System.out.println("Optimal : " + Math.round(optAvg * 1000.0) / 1000.0);
+        System.out.println("FIFO : " + Math.round(fifoAvg * 1000.0) / 1000.0);
+        System.out.println("LRU : " + Math.round(lruAvg * 1000.0) / 1000.0);
 
         System.out.printf("------------------------");
     }
